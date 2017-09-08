9b0edfd3275f5122c869e53f35c7efbe455a8666
Mariley20 <mariley2018@gmail.com>
2017-09-08 13:24:29 -0500

    ya terminado 1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,31 @@
 /*aquí va tu código*/
 
 $('#guardarDatos').click(() => {
-	console.log('hummm')
-	if (($("#clave").val() != "") && ($("#valor").val() != "")) {
-		localStorage.nombre = $("#clave").val();
-		localStorage.comentario = $("#valor").val();
-	
-		$('#comentarios').append(`<p>${localStorage.nombre}</p>\
-			<p>${localStorage.comentario}</p>`)
-	}
-	else
-	{
-		document.getElementById("datos").innerHTML = "No has introducido tu nombre y tu password";
-	}
-	
+    if (($("#clave").val() != "") && ($("#valor").val() != "")) {
+        let nombre = $('#clave').val();
+        let comentario = $('#valor').val();
+        localStorage.setItem(nombre, comentario);
+        $('#comentarios').append(`<div><label>${nombre}</label><br>\
+			<p>${comentario}</p></div>`)
+        //recoverData();
+    } else {
+        $("#datos").text("No has introducido tu nombre y tu comentario");
+    }
+
 });
 $('#limpiarDatos').click(() => {
-	localStorage.clear();
+    localStorage.clear();
 });
+
+function recoverData() {
+    if (localStorage.length >= 1) {
+        //for (let i = localStorage.length - 1; i >= 0 ; i--) {
+        for (var i = 0; i < localStorage.length; i++) {
+            let nombre = localStorage.key(i);
+            let comentario = localStorage.getItem(nombre);
+            $("#comentarios").append(`<div><label>${nombre}</label><br>\
+			<p>${comentario}</p></div>`);
+        }
+    }
+}
+recoverData();
